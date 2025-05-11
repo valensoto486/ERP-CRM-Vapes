ce68c9e1b309dd73e2efd5dda3beed6d66bf836c
valensoto486 <valensoto486@gmail.com>
2025-05-11 01:17:18 -0500

    feat: Configuracion de CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 # app/main.py
 from fastapi import FastAPI
 from app.routes import clientes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -10,3 +11,11 @@ app.include_router(clientes.router, prefix="/clientes", tags=["Clientes"])
 @app.get("/")
 def read_root():
     return {"message": "Bienvenido al CRM de Vaporizadores"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
